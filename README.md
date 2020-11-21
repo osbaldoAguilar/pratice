1a3baba9b85d7d6f8fa7f04fa526297d08b19d76
osbaldoAguilar <50221347+osbaldoAguilar@users.noreply.github.com>
2020-11-21 15:05:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# Web Basics 2: Week 1 Assignment
+
+This assignment will cover the following JavaScript topics:
+
+- Executing code in the terminal
+- Storing and tracking information with variables
+- Working with strings
+- Working with numbers
+- Making decisions in your code with conditional statements
+- Creating reusable code with functions
+- Passing information into functions
+- Using arrow functions
+
+---
+
+## Issues and Feedback
+
+Be encouraged to ask questions or point out issues in the assignment!
+
+Mentors will be happy to help out and your fellow students will benefit from your input.
+
+[Report an Issue](https://github.com/CodeTheDream/Web-Basics-2-Week-1/issues)
+
+---
+
+## Instructions
+
+### Getting Started:
+
+Clone this repository so that the code is available on your local machine:
+
+> **Note:** Run this command from the directory where you want to store your code (ex. `lessons/`)
+
+    git clone git@github.com:Code-the-Dream-School/web-basics-2-week-1-[your-username].git
+
+Navigate to the directory you just cloned in your terminal:
+
+    cd web-basics-2-week-1-[your-username]
+
+Create a new local branch to work on separate from the mainline branch (`master`):
+
+    git checkout -b assignment
+
+Make sure that you have `node.js` installed on your machine so that you can run JavaScript in the terminal:
+
+    node -v
+
+If the output says `command not found`, then [download node.js](https://nodejs.org/en/download) and try the command again.
+
+Finally, open the project directory in your code editor and continue to the next section.
+
+### Task List:
+
+For this assignment, all changes will be made within the `index.js` file.
+
+- [ ] Step 1: Complete each of the challenges outlined in the file comments
+- [ ] Step 2: Execute the JavaScript you wrote by opening the terminal and running `node index.js`
+- [ ] Step 3: Validate the output of all your challenge answers
+
+### Final Step:
+
+Check the status of your local repository to double-check the changes you made:
+
+    git status
+
+Stage the JavaScript file that you edited:
+
+    git add index.js
+
+Create a commit for the changes you made and add a message:
+
+    git commit -m "your message"
+
+Push your branch to the remote repository (visible in GitHub):
+
+    git push origin assignment
+
+Create a Pull Request in GitHub and request feedback:
+
+1. Navigate to the "Pull Requests" tab in your repository
+2. Click the "New pull request" button
+3. Set the base branch to `master` and the compare branch to `assignment`
+4. Click the "Create pull request" button
+5. Ask a mentor for feedback in Slack or add them as a reviewer
+
+Click the button to "Merge" your Pull Request and celebrate 🎉
+
+---
+
+Created by [Code the Dream](https://www.codethedream.org)
---
+++ index.js
@@ -0,0 +1,122 @@
+/* Example Challenge
+ * Print out "hello world" in the console
+ *
+ * Steps:
+ * 1. Use `console.log` to print out "hello world"
+ *
+ * Example Output: "hello world"
+ */
+
+console.log('hello world')
+
+/* Challenge #1
+ * Create a variable and print the value
+ *
+ * Steps:
+ * 1. Create a variable and assign it any value
+ * 2. Use `console.log` to print out your variable
+ *
+ * Example Output: n/a
+ */
+
+// TODO: ADD CODE HERE
+
+/* Challenge #2
+ * Create variables for your first and last name then print your full name
+ *
+ * Steps:
+ * 1. Create a variable called `firstName` and assign your first name as the string value
+ * 2. Create a variable called `lastName` and assign your last name as the string value
+ * 3. Use `console.log` to print out your full name (make sure there's a space!)
+ *
+ * Example Output: "Elizabeth Thompson"
+ */
+
+// TODO: ADD CODE HERE
+
+/* Challenge #3
+ * Calculate the total price rounded to two decimals
+ *
+ * Steps:
+ * 1. Create a variable called `price` and assign a float value (ex. 9.98)
+ * 2. Create a variable called `quantity` and assign an integer value (ex. 5)
+ * 3. Create a variable called `total` and assign the value of `price` multiplied by `quantity`
+ * 4. Use `console.log` to print out the `total` (rounded to the nearest two decimals)
+ *
+ * Example Output: "$49.90"
+ */
+
+// TODO: ADD CODE HERE
+
+/* Challenge #4
+ * Declare a variable and conditionally assign it a value
+ *
+ * Steps:
+ * 1. Create a variable called `weather` and assign "Rainy" as the string value
+ * 2. Declare a variable called `message` and don't assign it a value
+ * 3. Create a conditional statement that checks if `weather` is equal to "Sunny"
+ * 4. If `weather` meets the condition above, then set `message` equal to "Yay! It's sunny outside"
+ * 5. Add an `else` clause that sets `message` equal to "Aww! It's not sunny outside"
+ * 6. Use `console.log` to print out the value of `message`
+ *
+ * Example Output: "Aww! It's not sunny outside"
+ */
+
+// TODO: ADD CODE HERE
+
+/* Challenge #5
+ * Create a function that returns any string
+ *
+ * Steps:
+ * 1. Create a function called `myFunction` using the function keyword
+ * 2. Add a return statement in your function which returns a random string
+ * 3. Use `console.log` to print out the output of `myFunction()`
+ *
+ * Example Output: "I called a function!"
+ */
+
+// TODO: ADD CODE HERE
+
+/* Challenge #6
+ * Rewrite the function in Challenge #5 as an arrow function
+ *
+ * Steps:
+ * 1. Create a function called `myArrowFunction` using the arrow syntax
+ * 2. Add a return statement in your function which returns a random string
+ * 3. Use `console.log` to print out the output of `myArrowFunction()`
+ *
+ * Example Output: "I called an arrow function!"
+ */
+
+// TODO: ADD CODE HERE
+
+/* Challenge #7
+ * Create a function that accepts a string argument and returns it in all uppercase
+ *
+ * Steps:
+ * 1. Create a function called `stringToUpper`
+ * 2. Add a parameter called `str` in your function signature
+ * 3. Add a return statement in your function which returns the input string in all uppercase letters
+ * 4. Use `console.log` to print out the output of `stringToUpper(str)`
+ *
+ * Example Output: "WHY AM I SCREAMING"
+ */
+
+// TODO: ADD CODE HERE
+
+/* Challenge #8
+ * Create a function that accepts a number argument and returns double its value
+ *
+ * Steps:
+ * 1. Create a function called `multiplyByTwo`
+ * 2. Add a parameter called `number` in your function signature
+ * 3. Add a return statement in your function which returns `number` times two
+ * 4. Create a variable called `num1` and assign it to the result of `multiplyByTwo(1)`
+ * 5. Create a variable called `num2` and assign it to the result of `multiplyByTwo(5)`
+ * 6. Create a variable called `num3` and assign it to the result of `multiplyByTwo(10)`
+ * 7. Use `console.log` to print out the value of `num1`, `num2`, and `num3`
+ *
+ * Example Output: 2, 10, 20
+ */
+
+// TODO: ADD CODE HERE
